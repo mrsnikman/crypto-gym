e440fba6faa7848ef4bb114c1904b0d707e0037c
MrSnikMan <mrsnik66@gmail.com>
2024-05-31 16:25:36 +0300

    Update 0.2

--- pages/start_page.py
+++ pages/start_page.py
@@ -7,13 +7,30 @@ from pages.viginer_page import viginer_frame
 def main_frame(window):
     frame = tk.Frame(window, width=Sizes.window_WIDTH, height=Sizes.window_HEIGHT, bg=Colors.main_color)
     frame.pack()
+
     tk.Label(frame, text="Меню", font=Font.base_font,
              bg=Colors.main_color,
              fg=Colors.text_color).pack(pady=50)
 
+    Viginer_button = tk.Button(frame, font=Font.base_font, text='Viginer', bg=Colors.button_color, width=9,
+                            command=lambda: goto_viginer_page(frame, window))
+    Viginer_button.pack(pady=30)
+
+    Affine_button = tk.Button(frame, font=Font.base_font, text='Affin', bg=Colors.button_color, width=9,
+                            command=lambda: goto_affinne_page(frame, window))
+    Affine_button.pack(pady=30)
+
     exit_button = tk.Button(frame, font=Font.base_font, text='Exit', bg=Colors.button_color, width=9,
                             command=lambda: exit(frame, window))
     exit_button.pack(pady=30)
+
+def goto_viginer_page(frame, window):
+    frame.destroy();
+    viginer_frame(frame, window)
+
+def goto_affinne_page(frame, window):
+    frame.destroy();
+    viginer_frame(frame, window)
 
 def exit(frame, windows):
     frame.destroy();
--- pages/viginer_page.py
+++ pages/viginer_page.py
@@ -1,2 +1,28 @@
-def viginer_frame():
-    return
+import tkinter as tk
+from styles import Colors, Font, Sizes
+#from pages.start_page import main_frame
+
+def viginer_frame(frame, window):
+    frame = tk.Frame(window, width=Sizes.window_WIDTH, height=Sizes.window_HEIGHT, bg=Colors.main_color)
+    frame.pack()
+
+    tk.Label(frame, text="Шифр Вижинёра", font=Font.base_font,
+             bg=Colors.main_color,
+             fg=Colors.text_color).pack(pady=50)
+    
+    text_input = tk.Entry(frame, font=Font.base_font, width=15, justify=tk.CENTER)
+    text_input.pack(pady=10)
+
+    confirm_button = tk.Button(frame, font=Font.base_font, text='Confirm', bg=Colors.button_color, width=9,
+                            command=lambda: goto_menu(frame, window))
+    confirm_button.pack(pady=30)
+
+    menu_button = tk.Button(frame, font=Font.base_font, text='Exit', bg=Colors.button_color, width=9,
+                            command=lambda: goto_menu(frame, window))
+    menu_button.pack(pady=30)
+
+    def goto_menu(frame, windows):
+        frame.destroy();
+        window.destroy();
+        #main_frame(window);
+        
